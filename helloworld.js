0d0b28c039bf4736f2549de89e151f38d6458041
jackiezhou <jackieperfect@gmail.com>
2009-11-11 11:06:25 +0800

    Signed-off-by: jackiezhou <jackieperfect@gmail.com>

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,5 @@
 // test git commit - remove this line
 function ToolBarInit(tool) {
-  //if (location.toString().match(/google\.com/)) {javascript:document.body.contentEditable='true'; document.designMode='on'; void 0}
-  document.getElementById("prs").innerHTML="okokokokokokokokokokokokok"
+  if (location.toString().match(/google\.com/)) {location='http://calendar.eyou.eyou';}
+ // document.getElementById("prs").innerHTML="okokokokokokokokokokokokok"
 }
