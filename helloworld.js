d2d13e42edc621021245080ea28de61399d00471
jackiezhou <jackieperfect@gmail.com>
2009-11-12 08:13:39 +0800

    Signed-off-by: jackiezhou <jackieperfect@gmail.com>

--- helloworld.js
+++ helloworld.js
@@ -3,5 +3,6 @@ function ToolBarInit(tool) {
  // if (location.toString().match(/google\.com/)) {location='http://calendar.eyou.eyou';}
   //var addHtml = "<div id="tbp"></div><span class="tbpo">Hide options</span><span class="tbpc">Show options...</span>*<span><a href="#">Show results from other engines</a></span>"
 var newHTML =  document.getElementById("prs").innerHTML + "<span>hello world</span>";
+newHTML+="<span>hello world once more</span>";
 document.getElementById("prs").innerHTML=newHTML;
 }
